9150443892d611ffca5741a3a8405c8f1be93b55
MarkFridlin <158204105+MarkFridlin@users.noreply.github.com>
2024-02-08 20:14:19 -0500

    Update TipCalculator.py

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,6 +1,7 @@
 """ name = input('What is your name?\n')
 print(f'Hello {name}')
  """
-CBT = input('What is your cost before tip?')
-CBTf = float(CBT) 
-print(f'Your total cost is: {CBTf * 1.2}')
+while True:
+    CBT = input('What is your cost before tip?')
+    CBTf = float(CBT) 
+    print(f'Your total cost is: {round(CBTf * 1.2, 2)}')
