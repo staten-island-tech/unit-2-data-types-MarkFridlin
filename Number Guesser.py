6be7d161667cf8846cd01e2006fb6829f64f4f29
MarkFridlin <markf57@nycstudents.net>
2024-02-12 12:27:36 -0500

    2/12/24

--- Number Guesser.py
+++
@@ -1,5 +0,0 @@
-import random
-
-random.randint(1,10)
-
-
--- gcf.py
+++ gcf.py
@@ -4,19 +4,12 @@ def print_factors(x):
        if x % i == 0:
            factors_list.append(i)
     return factors_list
-
 num1 = int(input("What is your first number: "))
-
 factors_num1 = print_factors(num1)
-
 num2 = int(input("What is your second number: "))
-
 factors_num2 = print_factors(num2)
-
 gcf = 1
 for num in factors_num1:
     if num in factors_num2 and gcf < num:
         gcf = num 
-
 print("The greatest common factor of ", num1 , ' and', num2, "are: ", gcf)
-
